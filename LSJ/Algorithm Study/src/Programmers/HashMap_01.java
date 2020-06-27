fbd5344006a38e3e4dd10672af8c4f8494db4d2d
suji-l <65217717+suji-l@users.noreply.github.com>
2020-06-27 11:41:01 +0900

    Update HashMap_01.java

--- LSJ/Algorithm Study/src/Programmers/HashMap_01.java
+++ LSJ/Algorithm Study/src/Programmers/HashMap_01.java
@@ -1,14 +1,32 @@
 package Programmers;
 
+import java.util.Arrays;
+
 public class HashMap_01 {
 	
 	    public String solution(String[] participant, String[] completion) {
 	        String answer = "";
-	        return answer;
+	        
+	        Arrays.sort(participant);
+	        Arrays.sort(completion);
+	        int i = 0;
+	        for (i=0; i<completion.length;i++) {
+	        	if(!participant[i].equals(completion)) {
+	        		return participant[i];
+	        		
+	        	}
+	        	
+	        }
+	        	        
+	        return participant[i];
 	        
 	        
 	    }
 
-	
+	    public String solution2(String[] participant, String[] completion) {
+	    	
+	    	String answer="";
+	    	return answer;
+	    }
 	
 }
