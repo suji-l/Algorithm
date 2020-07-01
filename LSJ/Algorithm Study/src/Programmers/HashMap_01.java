2d84b1cbab15d1df541b90ca85e4f7704aac319e
suji-l <65217717+suji-l@users.noreply.github.com>
2020-07-02 04:56:54 +0900

    0702 solution

--- LSJ/Algorithm Study/src/Programmers/HashMap_01.java
+++ LSJ/Algorithm Study/src/Programmers/HashMap_01.java
@@ -1,13 +1,16 @@
 package Programmers;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class HashMap_01 {
 	
 	    public String solution(String[] participant, String[] completion) {
-	        String answer = "";
-	        
-	        Arrays.sort(participant);
+	    	//Programmers> Hash> 완주하지 못한 선수
+	    	//Arrays.sort 오름차순 정렬 사용
+	    	//내림차순이나 원하는 대로 정렬조건을 달리 하려면 Comparable/ Comparator사용
+	       
+	    	Arrays.sort(participant);
 	        Arrays.sort(completion);
 	        int i = 0;
 	        for (i=0; i<completion.length;i++) {
@@ -22,11 +25,27 @@ public class HashMap_01 {
 	        
 	        
 	    }
-
+	    
+	    //HashMap사용
 	    public String solution2(String[] participant, String[] completion) {
-	    	
-	    	String answer="";
-	    	return answer;
+	       String answer="";
+	       
+	       HashMap<String, Integer> name = new HashMap<>();
+	       for(String player:participant) name.put(player, name.getOrDefault(player, 0)+1);
+	       for(String player:completion) name.put(player, name.get(player)-1);
+	       
+	       for(String key: name.keySet()) {
+	    	   if(name.get(key)!=0) {
+	    		   answer=key;
+	    	   }
+	       }
+	            
+	       
+	       return answer;
+	       
+	       
+	        
 	    }
-	
+	    
+
 }
---
+++ LSJ/Algorithm Study/src/Programmers/practice01.java
@@ -0,0 +1,24 @@
+package Programmers;
+
+public class practice01 {
+//수박수박수박수?문제
+//길이가 n이고, 수박수박수박수....와 같은 패턴을 유지하는 문자열을 리턴하는 함수, solution을 완성하세요. 
+//예를들어 n이 4이면 수박수박을 리턴하고 3이라면 수박수를 리턴하면 됩니다.
+	public String solution(int n) {
+
+		String answer = "";
+
+		for (int i = 1; i <= n; i++) {
+			if (i % 2 == 1) {
+				answer += "수";
+			} else {
+				answer += "박";
+			}
+
+		}
+
+		return answer;
+
+	}
+
+}
